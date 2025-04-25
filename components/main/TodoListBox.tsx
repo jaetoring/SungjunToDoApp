0891ed7324ecdc3dac3f01dd6fc12ef6adb24aa9
문성준 <115637701+CamelIsTheBestConvention@users.noreply.github.com>
2025-04-25 21:51:56 +0900

    Update TodoListBox.tsx

--- components/main/TodoListBox.tsx
+++ components/main/TodoListBox.tsx
@@ -20,7 +20,7 @@ const TodoListBox = ({ todoData }: TodoListBoxProps) => {
       <View className="w-full flex-col px-5 py-4">
         <Text className="text-3xl font-bold mb-4">TodoList</Text>
         <View className="w-full">
-          {todoData.map((todo, _) => (
+          {todoData.map((todo) => (
             <TodoBox todoProps={todo} />
           ))}
         </View>
